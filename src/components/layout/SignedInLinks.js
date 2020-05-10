1bd695d46205389251f509090a9bae83e4f21338
LuizFurtado <luizfpfurtado@hotmail.com>
2020-05-10 12:33:22 -0300

    Components done - redux added to the project

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -5,7 +5,7 @@ const SignedInLinks = () => {
     return (
         <nav className="nav-wrapper grey darken-3">
             <ul className="right">
-                <li><NavLink to='/'>New Project</NavLink></li>
+                <li><NavLink to='/create'>New Project</NavLink></li>
                 <li><NavLink to='/'>Log Out</NavLink></li>
                 <li><NavLink to='/' className="btn btn-floating pink lighten-1">LF</NavLink></li>
             </ul>
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -5,8 +5,8 @@ const SignedOutLinks = () => {
     return (
         <nav className="nav-wrapper grey darken-3">
             <ul className="right">
-                <li><NavLink to='/'>SignUp</NavLink></li>
-                <li><NavLink to='/'>Login</NavLink></li>
+                <li><NavLink to='/signup'>SignUp</NavLink></li>
+                <li><NavLink to='/signin'>Login</NavLink></li>
             </ul>
         </nav>
     )
